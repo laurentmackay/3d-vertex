9a7b9cde1b5a5602d328863845713e754cb14a3b
laurentmackay <55097896+laurentmackay@users.noreply.github.com>
2021-12-09 11:01:54 -0800

    Update setup.py
    added a github link for mayavi, since its so dynamic

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ from setuptools import setup, find_packages
 
 
 install_requires=['networkx','numpy>1.10','scipy','numba>=0.54', 'vtk==9.0.3','matplotlib', 'mayavi']
-
+dependency_links = ['git+https://github.com/enthought/mayavi.git@master']
 
 setup(name='3d-vertex',
       version='1.0',
       packages=find_packages(),
       install_requires=install_requires
+      dependency_links=dependency_links
       )
