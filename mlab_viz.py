76b1538e51d3c962c9cf649f2fe4a8f57025745d
laurentmackay <55097896+laurentmackay@users.noreply.github.com>
2021-12-10 12:49:35 -0800

    live edge_viewer
    added a asynchronous (multiprocessing) edge_viewer that can be updated on the fly

--- mlab_viz.py
+++ mlab_viz.py
@@ -1,8 +1,45 @@
 import networkx as nx
 import numpy as np
+import multiprocessing as mp
 from mayavi import mlab
+from util import run_dill, make_dill
+import time
+import vtk
 
-def view_edges(G, tube_radius=0.15, colormap='Blues', attr=None, color=None, radius_factor = 3):
+vtk.vtkObject.GlobalWarningDisplayOff()
+
+def edge_viewer(*args,**kwargs):
+    a,b = mp.Pipe(duplex=True)
+    man = _view_edges(*args,**kwargs)
+    proc = mp.Process(target=run_dill, args=make_dill(man, b), daemon=True)
+    proc.start()
+    return a.send
+
+def _view_edges(*a,**kw):    
+    def inner(b):
+        pts = edge_view(*a,**kw)
+        @mlab.animate(delay=16,ui=False)
+        def anim():
+            while True:
+                if b.poll():
+                    while b.poll():
+                        msg=b.recv()
+                    edge_view(msg, *a[1:-1], pts=pts, **kw)
+                
+                yield
+        an=anim()
+        mlab.show()
+
+    return inner
+
+
+
+def edge_view(G, pts=None, tube_radius=0.15, colormap='Blues', attr=None, color=None, radius_factor = 3):
+    draw = pts is None
+    if draw:
+        mlab.figure(figure=1, bgcolor=(0.3,0.3,0.3), size=(800, 600))
+    else:
+        pts.scene.disable_render = True
 
     pos = nx.get_node_attributes(G,'pos')
 
@@ -10,7 +47,8 @@ def view_edges(G, tube_radius=0.15, colormap='Blues', attr=None, color=None, rad
     y = []
     z = []
     connections = []
-    s = []
+    if attr is not None:
+        s = []
     
 
     for i, e in enumerate(G.edges()):
@@ -24,31 +62,56 @@ def view_edges(G, tube_radius=0.15, colormap='Blues', attr=None, color=None, rad
         connections.append(np.array([2*i,2*i+1]))
         if attr is not None:
             scalar = G.get_edge_data(*e)[attr]
-        else:
-            scalar = 0 
-        s.extend((scalar,scalar))
+            s.extend((scalar,scalar))
 
+        
 
     x = np.hstack(x)
     y = np.hstack(y)
     z = np.hstack(z)
     s = np.hstack(s)
-
-    pts = mlab.points3d(x,y,z,s,
-                        scale_factor=0.1,
-                        scale_mode='none',
-                        colormap=colormap,
-                        resolution=20)
-
+    
+    if draw:
+        if attr is not None:
+            args = (x,y,z,s)
+        else:
+            args= (x,y,z)
+        pts = mlab.pipeline.scalar_scatter(*args,
+                            scale_factor=0.1,
+                            scale_mode='none',
+                            colormap=colormap,
+                            resolution=20)
+    else:
+        pts.mlab_source.set(x=x,y=y,z=z,scalars=s)
 
     pts.mlab_source.dataset.lines = np.vstack(connections)
     
-    tube = mlab.pipeline.tube(pts, tube_radius=tube_radius)
-    tube.filter.radius_factor = radius_factor
-    tube.filter.vary_radius = 'vary_radius_by_scalar'
-    mlab.pipeline.surface(tube, color=color)
+   
+    if draw:
+        
+        tube = mlab.pipeline.tube(pts, tube_radius=tube_radius)
+        mlab.pipeline.surface(tube, color=color)
+        tube.filter.radius_factor = radius_factor
+        tube.filter.vary_radius = 'vary_radius_by_scalar'
+        # pts.remove()
 
-    pts.remove()
 
-    mlab.show()
+    pts.scene.disable_render = False
+    if not draw:
+        pts.scene.render()
+        # 
     
+   
+
+        
+
+    return pts
+    
+    
+
+  #start visualisation loop in the main-thread, blocking other executions
+
+        # loopThread.join()
+
+
+
--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 
 
-install_requires=['networkx','numpy>1.10','scipy','numba>=0.54', 'vtk==9.0.3','matplotlib', 'mayavi']
-dependency_links = ['git+https://github.com/enthought/mayavi.git@master']
+install_requires=['networkx','numpy>1.10','scipy','numba>=0.54', 'vtk==9.0.3','matplotlib', 'mayavi','dill']
+dependency_links = ['git+https://github.com/enthought/mayavi.git@master','git+https://github.com/uqfoundation/dill.git@master']
 
 setup(name='3d-vertex',
       version='1.0',
       packages=find_packages(),
-      install_requires=install_requires
+      install_requires=install_requires,
       dependency_links=dependency_links
       )
