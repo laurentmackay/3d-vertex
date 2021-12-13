915019427dd5b7660ab2c6e2978e7f9640b5a212
Laurent MacKay <lmackay@math.ubc.ca>
2021-12-13 13:25:28 -0800

    fixed some typing and runtime errors during intercalation
    blow up is happening now though :(

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from vertex_3d import *
 import SG
-from mlab_viz import edge_viewer
+# from mlab_viz import edge_viewer
 
 
 if __name__ == '__main__':
@@ -10,14 +10,14 @@ if __name__ == '__main__':
 
     #initialize some things for the callback
     invagination = SG.invagination(G, belt)
-    viewer = edge_viewer(G,attr='myosin')
+    # viewer = edge_viewer(G,attr='myosin')
     t_last = 0 
     t_plot = 5
 
     def callback(t):
         invagination(t)
-        if t-t_last>=t_plot:
-            viewer(G)
+        # if t-t_last>=t_plot:
+        #     viewer(G)
 
     #create integrator
     integrate = vertex_integrator(G, K, centers, num_api_nodes, circum_sorted, belt, triangles, pre_callback=callback)
