540df215960a33dcc34bf90b9b557d239beb868a
Laurent MacKay <lmackay@math.ubc.ca>
2021-12-14 00:37:16 -0800

    added windows-specific and wsl-specific checks to algorithm and setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import sys
 import os
+from platform import uname
 from setuptools import setup, find_packages
 
+IS_LINUX = sys.platform.startswith('linux')
+IS_WINDOWS = sys.platform.startswith('win')
+
+def in_wsl() -> bool:
+    return 'microsoft-standard' in uname().release
 
 
-install_requires=['networkx','numpy>1.10','scipy','numba>=0.54', 'vtk==9.0.3','matplotlib', 'mayavi','dill']
-dependency_links = ['git+https://github.com/enthought/mayavi.git@master','git+https://github.com/uqfoundation/dill.git@master']
+install_requires=['networkx','numpy>1.10','scipy','numba>=0.54', 'vtk==9.0.3','matplotlib']
+dependency_links =[]
+
+
+if not in_wsl():
+      install_requires.append('mayavi')
+      dependency_links.append('git+https://github.com/enthought/mayavi.git@master')
+
+if IS_WINDOWS:
+      install_requires.append('dill')
+      dependency_links.append('git+https://github.com/uqfoundation/dill.git@master')
 
 setup(name='3d-vertex',
       version='1.0',
--- util.py
+++ util.py
@@ -1,9 +1,11 @@
-import dill
+import sys
+IS_WINDOWS = sys.platform.startswith('win')
 
+if IS_WINDOWS:
+    import dill
+    def run_dill(payload, args):
+        fun = dill.loads(payload)
+        return fun(*args)
 
-def run_dill(payload, args):
-    fun = dill.loads(payload)
-    return fun(*args)
-
-def make_dill(f,a):
-    return (dill.dumps(f),(a,))
+    def make_dill(f,a):
+        return (dill.dumps(f),(a,))
